08cc26480f3e2d6b4e4d0fb3f130d538f30c7edb
aidanlokeeffe <42976464+aidanlokeeffe@users.noreply.github.com>
2021-06-10 09:35:27 -0400

    Some updates to IS.py

--- IS_age.py
+++ IS_age.py
@@ -3,7 +3,85 @@ import random
 from Package import Package
 from Pitstop import Pitstop
 
+# Components of special pitstops
+def z1(x):
+	return 0
+def z2(x):
+	return []
+def z3(x):
+	return []
+
+def o1(x):
+	return x
+def o2(x):
+	return x
+def make_o3j(j):
+	def o3(x):
+		try:
+			return [ x[0] + [j] ]
+		except IndexError:
+			return [[j]]
+	return o3
+
+# Special pitstops themselves
+zero = Pitstop(z1,z2,z3)
+def one_j(j):
+	return Pitstop(o1, o2, make_o3j(j))
+
+# Array multiplication function
+def array_mult(pkg_arr, pit_arr):
+	N = pkg_arr.shape[0] 
+	assert(N == pit_arr.shape[0])
+
+	out = []
+	for j in range(N):
+		component = zero
+		for i in range(N):
+			component = component + pit_arr[i,j].apply(pkg_arr[i])
+		out.append(component)
+	return out
+
+def make_xi(N):
+	nodes = list(range(N))
+
+def xi(N, tag):
+	j = random.sample( list(range(N)))
+
+
+
+
+
+
+
+
+
 # Read the connectome data
+# For now, we'll just have a dummy matrix
+adj = np.array([[0,0,0,0,0,0,1],
+ 				[0,0,0,1,0,0,0],
+ 				[1,0,0,0,1,0,0],
+ 				[0,0,1,0,0,0,0],
+ 				[1,1,0,0,0,1,0],
+ 				[1,1,0,0,0,1,0],
+ 				[0,1,1,0,0,0,0],
+ 				[1,0,0,1,0,0,0]])
+
+# We will be able to get N when we read the file. For now, it will be hard coded
+N = 7
+
+pits = []
+for i in range(7):
+	row = []
+	for j in range(7):
+		if adj[i,j]:
+			pits.append(one_j(j))
+			continue
+		pits.append(zero)
+
+
+
+
+
 
 # Replace all the elements with pitstops
 
