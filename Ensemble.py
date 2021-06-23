a5139473c39289f958e1d2456234dc8e507fa44b
aidanlokeeffe <42976464+aidanlokeeffe@users.noreply.github.com>
2021-06-23 11:13:09 -0400

    completed ensemble class (AFAICT)

--- Ensemble.py
+++ Ensemble.py
@@ -1,3 +1,5 @@
+import random
+
 class Ensemble(object):
 
 	def __init__(self, adj):
@@ -22,22 +24,35 @@ class Ensemble(object):
 			print("Use a matrix with different memory than the seed matrix.")
 			raise AssertionError
 
-		# Ranodmly choose i, and then j such that mtx[i][j] = 1
-		# Ranodmly choose k, and then l such that mtx[k][l] = 1
+		abort_counter = 100
+		seeking = True
 
-		if (mtx[k][j]) or (mtx[i][l]):
-			continue
+		while abort_counter > 0 and seeking:
+			# Ranodmly choose i, and then j such that mtx[i][j] = 1
+			i = random.sample(range(self.N), 1)
+			valid_j = [j for j in range(self.N) if mtx[i][j]]
+			j = random.sample(valid_j, 1)
 
-		mtx[i][j] = 0
-		mtx[k][l] = 0
-		mtx[k][j] = 1
-		mtx[i][l] = 1
+			# Ranodmly choose k, and then l such that mtx[k][l] = 1
+			k = random.sample(range(self.N), 1)
+			valid_l = [l for l in range(self.N) if mtx[k][l]]
+			l = random.sample(valid_l, 1)
+
+
+			if (mtx[k][j]) or (mtx[i][l]):
+				abort_counter -= 1
+				continue
+
+			mtx[i][j] = 0
+			mtx[k][l] = 0
+			mtx[k][j] = 1
+			mtx[i][l] = 1
+			seeking = False
 
 	def create_member(self):
 		member = [ self.seed[i][j] for i in range(self.N) for j in range(self.N) ]
 
-		# Get the number of rewirings needed
+		for _ in range(self.num_iter):
+			self.rewire(member)
 
-		# Rewire that many times
-
-		# Append to ensemble
+		self.ensemble.append(member)
