0d8e93071f581bdb8729e3edc77e4ff521f3ad94
Jack Reever <73250083+jackreever@users.noreply.github.com>
2021-06-24 15:57:24 -0400

    added notes from the meeting and edited documentation in barch_animator

---
+++ notes_from_meeting.py
@@ -0,0 +1,10 @@
+#start by just running 100 steps and low load and see how much slower it gets 
+#run at load 1, .01, .05, .1, .2 for 500-1000+ steps, find avg age
+#if a message is represented by where it has been, 5 100 21 is age of 3
+#EVERYBODY GETS ONTO NETWORK WITH AGE 1. EVERY HOP, ADD ONE YEAR. DEATH IS A HOP, IT IS A YEAR
+#let it run for like 200 to get out of the initial transient, into a steady state. THEN start keeping track of tings 
+#1000 steps throwing out 500, but that's 'overkill'
+#run it for 1000 steps and keep track of any numerical statistic I want to track and see that things fluctuate 
+#LOOKING AT THE AFOREMENTIONED STAT: the beginning weird bit is the transient. get rid of the transient
+#cut off the first 5 to 20% 
+#gold standard is 500 to 1000 steps
