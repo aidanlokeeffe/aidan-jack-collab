041870f4755ee22073e253d04792c951fd64885b
aidanlokeeffe <42976464+aidanlokeeffe@users.noreply.github.com>
2021-06-28 15:45:59 -0400

    Two new output functions per Dr. Hao's request, and explanatory paragraph comments

--- Experiment.py
+++ Experiment.py
@@ -1,3 +1,33 @@
+'''
+Okay, this is the big class. In this explanation, we will use concepts from the
+Package and Container classes freely. 
+
+An experiment should be thought of as the whole shebang. The setup, the 
+simulation, and the analysis. Accordingly, there are several sections of
+methods in this class file. At the time of writing this comment, those 
+sections are:
+* SETUP METHODS
+* DYNAMIC METHODS
+* DATA FORMATTING MATHODS
+* OUTPUT METHODS
+
+The SETUP methods set the initial conditions. There is a helper function that
+reads in an adjacency matrix. Note that the adjacency matrix is then an 
+attribute of an experiment. (A comment for Jack and myself: We may want to give 
+the user a choice for using a csv or a 2-D array, since this will mesh better 
+with the ensemble class).
+
+There are two DYNAMIC function: advance and execute. advance takes a parameter 
+t so it can pass it to Container.fill. advance goes to the time t+1, and
+records all the attempted and actual activity, as well as the ages, and the 
+deaths. execute iterates advance up to the user provided time horizon.
+
+All of the DATA FORMATTING functions are helpers to the OUTPUT functions. I 
+won't say too much on these here, as that part of the code is currently
+undergoing a lot of changes.
+'''
+
+
 import numpy as np
 import math
 from Container import Container
@@ -5,7 +35,9 @@ from Package import Package
 
 class Experiment(object):
     ##############################
+    ##############################
     # SETUP METHODS
+    ##############################
     ##############################
     def __init__(self, fileName, load, T, choice=0):
         # Inputs
@@ -24,6 +56,8 @@ class Experiment(object):
         self.actual = []
         self.ages = []
         self.deaths = []
+
+        self.node_hists = []
         
         # NOTE: THESE WILL BE UPDATED INSIDE OF THE FUNCTION COLLIDER, EXCEPT FOR AT TIME 0
         # This will always work for attempted
@@ -34,6 +68,14 @@ class Experiment(object):
         self.ages.append([len(self.state.contents[j].vals[1]) for j in range(self.N)])
         # This will be an array of arrays of dead packages, with one inner array per timestep            
         self.deaths.append([]) 
+
+        next_node_hist = []
+        for j in range(self.N):
+            try:
+                next_node_hist.append( self.state.contents[j].vals[0][0] )
+            except IndexError:
+                next_node_hist.append(None)
+        self.node_hists.append( next_node_hist )
         
         if choice==0:
             self.propagate = self.state.RW_propagate
@@ -50,8 +92,13 @@ class Experiment(object):
         return None
 
 
+
+
+
+    ##############################
     ##############################
     # DYNAMIC METHODS
+    ##############################
     ##############################
     def advance(self, t):
         # Propagate the messages according to the chosen rule
@@ -87,6 +134,14 @@ class Experiment(object):
 
         self.ages.append(survivor_ages)
 
+        next_node_hist = []
+        for j in range(self.N):
+            try:
+                next_node_hist.append( self.state.contents[j].vals[0][0] )
+            except IndexError:
+                next_node_hist.append(None)
+        self.node_hists.append( next_node_hist )
+
     def execute(self):
         print("HERE ARE THE STATES")
         print( "t = 0: " + str(self.state) )
@@ -96,12 +151,49 @@ class Experiment(object):
         return (self.state, self.attempted, self.actual, self.ages, self.deaths)
 
 
+
+
+    ##############################
+    ##############################
+    # DATA FORMATTING METHODS
+    ##############################
+    ##############################
+
     # Age average stuff
     def timewise_average_age(self):
         out = {}
         for t in range(self.T):
             out[t] = sum( self.ages[t] )/self.N
         return out
+
+    def timewise_max_age(self):
+        out = {}
+        for t in range(self.T):
+            out[t] = max(self.ages[t])
+        return out
+
+
+    # Age at death stuff
+    def average_death_age(self):
+        num_deaths = 0
+        age_sum = 0
+
+        for tally in self.deaths:
+            for pkg in tally:
+                L = len(pkg.vals[0])
+                for i in range(L):
+                    num_deaths += 1
+                    age_sum += len( pkg.vals[1][i] )
+
+        if num_deaths == 0:
+            return -1
+        return age_sum / num_deaths
+
+
+
+
+
+
 
 
 
@@ -138,9 +230,10 @@ class Experiment(object):
 
 
 
-
+    ##############################
     ##############################
     # OUTPUT METHODS
+    ##############################
     ##############################
 
     # Basic outputs
@@ -205,29 +298,10 @@ class Experiment(object):
         return None
 
 
-
-    # Average age outputs
-    '''
-    def write_nodewise_average_age_csv(self, out_name):
-        # Get this data
-        node_averages = self.nodewise_average_age()
-
-        out_file = open(out_name, "w")
-
-        # Write the top line
-        out_file.write("Node, Average Age\n")
-
-        # Write the rest of the data
-        for j in range(self.N):
-            out_file.write(str(j) + ", " + str(node_averages[j]) + "\n")
-
-        out_file.close()
-    '''
-
     def write_timewise_average_age_csv(self, out_name):
         # Get this data
         time_averages = self.timewise_average_age()
-        print(time_averages)
+        #print(time_averages)
 
         out_file = open(out_name, "w")
 
@@ -240,7 +314,20 @@ class Experiment(object):
 
         out_file.close()
 
+    def write_timewise_max_age_csv(self, out_name):
+        # Get this data
+        time_maxes = self.timewise_max_age()
 
+        out_file = open(out_name, "w")
+
+        # Write the top line
+        out_file.write("Time, Maximum Age\n")
+
+        # Write the rest of the data
+        for t in range(self.T):
+            out_file.write( str(t) + ", " + str(time_maxes[t]) + "\n" )
+
+        out_file.close()
 
 
 
@@ -283,4 +370,56 @@ class Experiment(object):
         out_file.close()
 
         return None
+
+
+    def write_verif_output_1(self, out_name):
+        out_file = open(out_name, "w")
+
+        # Write the first line
+        out_file.write( "Time of Death, Message ID, Age at Death, Place of Death, Place Before Death\n" )
+
+        for t in range(self.T):
+            tally = self.deaths[t]
+            for pkg in tally:
+                N = len(pkg.vals[0])
+                for i in range(N):
+                    msg_id = str( pkg.vals[0][i] )
+                    age = str( len( pkg.vals[1][i] ) )
+                    place_of_death = str( pkg.vals[1][i][-1] )
+                    try:
+                        send_to_death = str( pkg.vals[1][i][-2] )
+                    except IndexError:
+                        send_to_death = str( pkg.vals[1][i][-1] )
+
+                    out_file.write( str(t) + ", " + msg_id + ", " + age + ", " + place_of_death + ", " + send_to_death + "\n" )
+
+        out_file.close()
+
+    def write_verif_output_2(self, out_name):
+        out_file = open(out_name, "w")
+
+        # Write the first line
+        st = "Time, "
+        for i in range(self.N):
+            st += str(i) + ", "
+        out_file.write(st[:-2] + "\n")
+
+        for t in range(self.T):
+            st = str(t) + ", "
+            for j in range(self.N):
+                st += str( self.node_hists[t][j] ) + ", "
+            out_file.write(st[:-2] + "\n")
+
+        out_file.close()
+
+
+
+
+
+
+
+
+
+
+
 
